e866819e92391f73d8a900f27a90262bc138d4ce
Mike7704 <Michael.cowley2001@gmail.com>
2024-02-07 18:55:40 +0000

    Added styling to main menu and made buttons function.

--- client/main.js
+++ client/main.js
@@ -1,1 +1,20 @@
+const playButton = document.getElementById("play-quiz");
+const leaderboardButton = document.getElementById("leaderboard");
+const loginButton = document.getElementById("login");
 
+// Give each main menu button a listener to load a page
+function setupMenuButtons() {
+  playButton.addEventListener("click", () => {
+    window.location.href = "quiz-setup.html";
+  });
+
+  leaderboardButton.addEventListener("click", () => {
+    window.location.href = "leaderboard.html";
+  });
+
+  loginButton.addEventListener("click", () => {
+    window.location.href = "login.html";
+  });
+}
+
+setupMenuButtons();
