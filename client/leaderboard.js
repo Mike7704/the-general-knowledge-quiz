9b6bf2c795be9dd8839c1a61c535f7ae9573a7af
Mike7704 <Michael.cowley2001@gmail.com>
2024-02-08 08:51:06 +0000

    Added some CSS and HTML elements to the login and leaderboard page. Back buttons return to main menu. More work is needed on the styling.

---
+++ client/leaderboard.js
@@ -0,0 +1,10 @@
+const backButton = document.getElementById("back-button");
+
+// Give each main menu button a listener to load a page
+function setupMenuButtons() {
+  backButton.addEventListener("click", () => {
+    window.location.href = "index.html";
+  });
+}
+
+setupMenuButtons();
