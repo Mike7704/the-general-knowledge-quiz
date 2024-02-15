b3bf6a429bbcbbe51ff6a61dfda1266cd6471b87
Mike7704 <Michael.cowley2001@gmail.com>
2024-02-15 12:05:48 +0000

    Attempt 10 at fixing VITE_ServerURL undefined error.

--- client/main.js
+++ client/main.js
@@ -2,6 +2,9 @@ const playButton = document.getElementById("play-quiz");
 const leaderboardButton = document.getElementById("leaderboard");
 const loginButton = document.getElementById("login");
 
+const baseURL = import.meta.env.VITE_SERVERURL;
+console.log(baseURL);
+
 // Give each main menu button a listener to load a page
 function setupMenuButtons() {
   playButton.addEventListener("click", () => {
